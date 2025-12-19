547739d02fff4b9bf088855b740b2aeebc37bcc6
yancao77 <ycao77@stanford.edu>
2025-12-19 10:42:34 -0800

    update base url

--- constants/apiRoutes.js
+++ constants/apiRoutes.js
@@ -29,56 +29,56 @@ export const GET_VARIABLES_BY_STUDY = `${BASE_URL}/api/entity/v1/search/variable
 export const SEARCH_VARIABLES = `${BASE_URL}/api/search/v1/variables`;
 
 // USER SUPPORT REQUEST FORM API CALL
-export const POST_SUPPORT_REQUEST = `$${BASE_URL}/api/user/v1/support-request/submit`;
-export const GET_REQUEST_TYPES = `$${BASE_URL}/api/user/v1/support-request/request-types`;
+export const POST_SUPPORT_REQUEST = `${BASE_URL}/api/user/v1/support-request/submit`;
+export const GET_REQUEST_TYPES = `${BASE_URL}/api/user/v1/support-request/request-types`;
 
 // SUPPORT DASHBOARD API CALL
-export const GET_ALL_SUPPORT_REQUEST = `$${BASE_URL}/api/user/v1/support-request/all-support-requests?status=`;
-export const GET_SUPPORT_REQUEST_BY_ID = `$${BASE_URL}/api/user/v1/support-request/`;
+export const GET_ALL_SUPPORT_REQUEST = `${BASE_URL}/api/user/v1/support-request/all-support-requests?status=`;
+export const GET_SUPPORT_REQUEST_BY_ID = `${BASE_URL}/api/user/v1/support-request/`;
 
 
 // INTERNAL DASHBOARD API CALL
-export const GET_SUPPORT_REQUEST_BY_ID_INTERNAL = `$${BASE_URL}/api/user/v1/support-request/officer/`;
+export const GET_SUPPORT_REQUEST_BY_ID_INTERNAL = `${BASE_URL}/api/user/v1/support-request/officer/`;
 
 // SUPPORT TICKET API CALL
-export const GET_ALL_SUPPORT_STATUSES = `$${BASE_URL}/api/user/v1/support-request/all-statuses`;
-export const GET_ALL_SUPPORT_SEVERITY = `$${BASE_URL}/api/user/v1/support-request/all-severity`;
-export const GET_ALL_SUPPORT_RESOLUTION_TYPES = `$${BASE_URL}/api/user/v1/support-request/all-resolution-types`;
-export const UPDATE_DETAILED_SUPPORT_TICKET = `$${BASE_URL}/api/user/v1/support-request/update-support-request/`;
-export const GET_ALL_ASSIGNEES = `$${BASE_URL}/api/user/v1/support-request/all-assignees`;
+export const GET_ALL_SUPPORT_STATUSES = `${BASE_URL}/api/user/v1/support-request/all-statuses`;
+export const GET_ALL_SUPPORT_SEVERITY = `${BASE_URL}/api/user/v1/support-request/all-severity`;
+export const GET_ALL_SUPPORT_RESOLUTION_TYPES = `${BASE_URL}/api/user/v1/support-request/all-resolution-types`;
+export const UPDATE_DETAILED_SUPPORT_TICKET = `${BASE_URL}/api/user/v1/support-request/update-support-request/`;
+export const GET_ALL_ASSIGNEES = `${BASE_URL}/api/user/v1/support-request/all-assignees`;
 
 // SUBMITTER DASHBOARD API CALL
-export const GET_SUBMITTER_SUBMISSIONS = `$${BASE_URL}/api/submission-service/v1/getSubmissions`;
-export const DELETE_SUBMISSION = `$${BASE_URL}/api/submission-service/v1/deleteSubmission?submissionId=`;
+export const GET_SUBMITTER_SUBMISSIONS = `${BASE_URL}/api/submission-service/v1/getSubmissions`;
+export const DELETE_SUBMISSION = `${BASE_URL}/api/submission-service/v1/deleteSubmission?submissionId=`;
 export const DOWNLOAD_STUDY_UUIDS = `/api/download/v1/download/study-uuids?sessionId=`;
 
 // // DATA INGEST APIs
-export const GET_STUDIES = `$${BASE_URL}/api/submission-service/v1/getStudies`;
-export const POST_DI_SUBMISSION = `$${BASE_URL}/api/submission-service/v1/create-submission?studyId=`;
-export const GET_CATEGORIES = `$${BASE_URL}/api/submission-service/v1/getCategories`;
-export const POST_DI_UPLOAD = `$${BASE_URL}/api/submission-service/v1/uploadFiles/uploadFile?submissionId=`;
-export const DELETE_DI_FILE = `$${BASE_URL}/api/submission-service/v1/deleteFile?fileId=`;
-export const GET_SUBMISSION_INFO = `$${BASE_URL}/api/submission-service/v1/submissionInfo?submissionId=`;
-export const GET_UPLOADED_FILES = `$${BASE_URL}/api/submission-service/v1/uploadFiles/getFiles?submissionId=`;
-export const POST_CREATE_BUNDLES = `$${BASE_URL}/api/submission-service/v1/uploadFiles/createBundles?submissionId=`;
-export const GET_BUNDLES = `$${BASE_URL}/api/submission-service/v1/bundle/get?submissionId=`;
-export const POST_BUNDLES = `$${BASE_URL}/api/submission-service/v1/bundle/update`;
-export const POST_VALIDATE_SUBMISSION = `$${BASE_URL}/api/submission-service/v1/validateFiles/validate?submissionId=`;
-export const GET_VALIDATION_RESULTS = `$${BASE_URL}/api/submission-service/v1/validateFiles/getResults?submissionId=`;
-export const PUT_REPLACE_FILE = `$${BASE_URL}/api/submission-service/v1/replaceFile?fileId=`;
-export const POST_SUBMIT_SUBMISSION = `$${BASE_URL}/api/submission-service/v1/reviewAndSubmit/submit?submissionId=`;
-export const GET_BUNDLE_FILES = `$${BASE_URL}/api/submission-service/v1/bundle/getFiles?fileId=`;
-export const DELETE_BUNDLE = `$${BASE_URL}/api/submission-service/v1/bundle/delete?fileId=`;
-export const POST_DI_ACKNOWLEDGEMENT = `$${BASE_URL}/api/submission-service/v1/validateFiles/acknowledge?submit=`;
-export const POST_DI_MULTI_UPLOAD = `$${BASE_URL}/api/submission-service/v1/uploadFiles/multiple?submissionId=`;
+export const GET_STUDIES = `${BASE_URL}/api/submission-service/v1/getStudies`;
+export const POST_DI_SUBMISSION = `${BASE_URL}/api/submission-service/v1/create-submission?studyId=`;
+export const GET_CATEGORIES = `${BASE_URL}/api/submission-service/v1/getCategories`;
+export const POST_DI_UPLOAD = `${BASE_URL}/api/submission-service/v1/uploadFiles/uploadFile?submissionId=`;
+export const DELETE_DI_FILE = `${BASE_URL}/api/submission-service/v1/deleteFile?fileId=`;
+export const GET_SUBMISSION_INFO = `${BASE_URL}/api/submission-service/v1/submissionInfo?submissionId=`;
+export const GET_UPLOADED_FILES = `${BASE_URL}/api/submission-service/v1/uploadFiles/getFiles?submissionId=`;
+export const POST_CREATE_BUNDLES = `${BASE_URL}/api/submission-service/v1/uploadFiles/createBundles?submissionId=`;
+export const GET_BUNDLES = `${BASE_URL}/api/submission-service/v1/bundle/get?submissionId=`;
+export const POST_BUNDLES = `${BASE_URL}/api/submission-service/v1/bundle/update`;
+export const POST_VALIDATE_SUBMISSION = `${BASE_URL}/api/submission-service/v1/validateFiles/validate?submissionId=`;
+export const GET_VALIDATION_RESULTS = `${BASE_URL}/api/submission-service/v1/validateFiles/getResults?submissionId=`;
+export const PUT_REPLACE_FILE = `${BASE_URL}/api/submission-service/v1/replaceFile?fileId=`;
+export const POST_SUBMIT_SUBMISSION = `${BASE_URL}/api/submission-service/v1/reviewAndSubmit/submit?submissionId=`;
+export const GET_BUNDLE_FILES = `${BASE_URL}/api/submission-service/v1/bundle/getFiles?fileId=`;
+export const DELETE_BUNDLE = `${BASE_URL}/api/submission-service/v1/bundle/delete?fileId=`;
+export const POST_DI_ACKNOWLEDGEMENT = `${BASE_URL}/api/submission-service/v1/validateFiles/acknowledge?submit=`;
+export const POST_DI_MULTI_UPLOAD = `${BASE_URL}/api/submission-service/v1/uploadFiles/multiple?submissionId=`;
 export const GET_DOWNLOAD_BY_FILE = `/api/submission-service/v1/download/validationErrorsByFile?fileId=`;
 export const GET_DOWNLOAD_BY_SUBMISSION = `/api/submission-service/v1/download/validationErrorsBySubmission?submissionId=`;
-export const POST_SAVE_VALIDATION = `$${BASE_URL}/api/submission-service/v1/validateFiles/acknowledge?submit=false`;
-export const POST_PREVIOUS_PAGE = `$${BASE_URL}/api/submission-service/v1/bundle/previousPage?submissionId=`;
-export const DELETE_MULTIPLE_DI = `$${BASE_URL}/api/submission-service/v1/deleteFiles?fileIds=`;
+export const POST_SAVE_VALIDATION = `${BASE_URL}/api/submission-service/v1/validateFiles/acknowledge?submit=false`;
+export const POST_PREVIOUS_PAGE = `${BASE_URL}/api/submission-service/v1/bundle/previousPage?submissionId=`;
+export const DELETE_MULTIPLE_DI = `${BASE_URL}/api/submission-service/v1/deleteFiles?fileIds=`;
 
 // INTERNAL DASHBOARD
-export const DOWNLOAD_SUPPORT_REQUEST_REPORT = `$${BASE_URL}/api/user/v1/support-request/download-support-request-report`;
+export const DOWNLOAD_SUPPORT_REQUEST_REPORT = `${BASE_URL}/api/user/v1/support-request/download-support-request-report`;
 
 // METRICS REPORTS APIS
 export const GET_HUB_CONTENT = `${BASE_URL}/api/report/v1/hubContent?aggBy=[aggBy]&reportId=[reportId]`;
@@ -95,54 +95,54 @@ export const GET_USER_ACTIVITIES = `${BASE_URL}/api/report/v1/userActivities?sta
 export const GET_USER_ACTIVITIES_CSV = `${BASE_URL}/api/report/v1/userActivitiesCSV?startDate=[startDate]&endDate=[endDate]`;
 
 // USER REGISTRATION
-export const GET_USER_RAS_INFO = `$${BASE_URL}/api/user/v1/getRegistrationDetails?sessionId=[sessionId]`;
-export const GET_RESEARCHER_LEVELS = `$${BASE_URL}/api/user/v1/user/researcher-levels`;
-export const GET_CENTERS = `$${BASE_URL}/api/user/v1/user/centers`;
-export const GET_APPROVED_INSTITUTIONS = `$${BASE_URL}/api/user/v1/user/approved-institutions`;
-export const POST_USER_REGISTRATION = `$${BASE_URL}/api/user/v1/user/user-registration?sessionId=[sessionId]`;
-export const POST_INSTITUTION = `$${BASE_URL}/api/user/v1/user/create-institution`;
-export const ALL_STATES = `$${BASE_URL}/api/user/v1/user/states`;
-export const ALL_COUNTRIES = `$${BASE_URL}/api/user/v1/user/countries`;
-export const GET_INSTITUTIONS_TYPES = `$${BASE_URL}/api/user/v1/user/institution-types`;
-export const GET_REFERRERS = `$${BASE_URL}/api/user/v1/user/referrer-types`;
+export const GET_USER_RAS_INFO = `${BASE_URL}/api/user/v1/getRegistrationDetails?sessionId=[sessionId]`;
+export const GET_RESEARCHER_LEVELS = `${BASE_URL}/api/user/v1/user/researcher-levels`;
+export const GET_CENTERS = `${BASE_URL}/api/user/v1/user/centers`;
+export const GET_APPROVED_INSTITUTIONS = `${BASE_URL}/api/user/v1/user/approved-institutions`;
+export const POST_USER_REGISTRATION = `${BASE_URL}/api/user/v1/user/user-registration?sessionId=[sessionId]`;
+export const POST_INSTITUTION = `${BASE_URL}/api/user/v1/user/create-institution`;
+export const ALL_STATES = `${BASE_URL}/api/user/v1/user/states`;
+export const ALL_COUNTRIES = `${BASE_URL}/api/user/v1/user/countries`;
+export const GET_INSTITUTIONS_TYPES = `${BASE_URL}/api/user/v1/user/institution-types`;
+export const GET_REFERRERS = `${BASE_URL}/api/user/v1/user/referrer-types`;
 
 // USER DASHBOARD
-export const GET_ALL_USERS = `$${BASE_URL}/api/user/v1/user/admin/users?status=`;
-export const GET_ALL_USER_ROLES = `$${BASE_URL}/api/user/v1/user/admin/roles`;
-export const GET_USER_BY_ID = `$${BASE_URL}/api/user/v1/user/admin/`;
-export const UPDATE_USER_INFO_BY_ID = `$${BASE_URL}/api/user/v1/user/admin/update/`;
-export const GET_ALL_GENERAL_STATUSES = `$${BASE_URL}/api/user/v1/user/admin/general-statuses`;
+export const GET_ALL_USERS = `${BASE_URL}/api/user/v1/user/admin/users?status=`;
+export const GET_ALL_USER_ROLES = `${BASE_URL}/api/user/v1/user/admin/roles`;
+export const GET_USER_BY_ID = `${BASE_URL}/api/user/v1/user/admin/`;
+export const UPDATE_USER_INFO_BY_ID = `${BASE_URL}/api/user/v1/user/admin/update/`;
+export const GET_ALL_GENERAL_STATUSES = `${BASE_URL}/api/user/v1/user/admin/general-statuses`;
 
 // USER PROFILE
-export const UPDATE_USER_PROFILE = `$${BASE_URL}/api/user/v1/user/editProfile`;
+export const UPDATE_USER_PROFILE = `${BASE_URL}/api/user/v1/user/editProfile`;
 
 // STUDY REGISTRATION FORM
 export const GET_CODELISTS = `${BASE_URL}/api/entity/v1/study/registrationCodelists`;
 export const GET_STUDY_ENTITIES = `${BASE_URL}/api/entity/v1/study/getRegistrationProperties`;
-export const GET_STUDY_VALUES = `$${BASE_URL}/api/submission-service/v1/study/getValues?studyId=[studyId]`;
-export const PUT_STUDY_REGISTRATION = `$${BASE_URL}/api/submission-service/v1/study/[userType]/edit?shouldSubmit=`;
+export const GET_STUDY_VALUES = `${BASE_URL}/api/submission-service/v1/study/getValues?studyId=[studyId]`;
+export const PUT_STUDY_REGISTRATION = `${BASE_URL}/api/submission-service/v1/study/[userType]/edit?shouldSubmit=`;
 
 // STUDY REGISTRATION DASHBOARD
-export const UPLOAD_STUDY_REG_DASH = `$${BASE_URL}/api/submission-service/v1/study/[userType]/create?shouldSubmit=`;
-export const DELETE_STUDY = `$${BASE_URL}/api/submission-service/v1/study/delete?studyId=[studyId]&deleteStudy=true`;
-export const DELETE_STUDY_FILES = `$${BASE_URL}/api/submission-service/v1/study/delete?studyId=[studyId]&deleteStudy=false`;
-export const GET_CURATOR_STUDIES = `$${BASE_URL}/api/submission-service/v1/study/curator/studies`;
-export const GET_CENTER_STUDIES = `$${BASE_URL}/api/submission-service/v1/study/center/studies`;
+export const UPLOAD_STUDY_REG_DASH = `${BASE_URL}/api/submission-service/v1/study/[userType]/create?shouldSubmit=`;
+export const DELETE_STUDY = `${BASE_URL}/api/submission-service/v1/study/delete?studyId=[studyId]&deleteStudy=true`;
+export const DELETE_STUDY_FILES = `${BASE_URL}/api/submission-service/v1/study/delete?studyId=[studyId]&deleteStudy=false`;
+export const GET_CURATOR_STUDIES = `${BASE_URL}/api/submission-service/v1/study/curator/studies`;
+export const GET_CENTER_STUDIES = `${BASE_URL}/api/submission-service/v1/study/center/studies`;
 
 // USER AUTH
-export const GET_INFO_BY_SESSION = `$${BASE_URL}/api/user/v1/user/infoBySession?sessionId=`;
-export const GET_INFO_BY_COOKIE = `$${BASE_URL}/api/user/v1/user/info`;
-export const UPDATE_SESSION_TOKEN = `$${BASE_URL}/api/user/v1/refresh/token`;
-export const USER_LOGOUT = `$${BASE_URL}/api/user/v1/logout`;
+export const GET_INFO_BY_SESSION = `${BASE_URL}/api/user/v1/user/infoBySession?sessionId=`;
+export const GET_INFO_BY_COOKIE = `${BASE_URL}/api/user/v1/user/info`;
+export const UPDATE_SESSION_TOKEN = `${BASE_URL}/api/user/v1/refresh/token`;
+export const USER_LOGOUT = `${BASE_URL}/api/user/v1/logout`;
 
 // CURATOR DASHBOARD
-export const GET_STUDY_FILE_SUBMISSIONS = `$${BASE_URL}/api/submission-service/v1/curator/getSubmissions`;
-export const GET_STUDY_FILE_SUBMISSION_FILES = `$${BASE_URL}/api/submission-service/v1/curator/getFilesBySubm?submissionId=`;
-export const POST_STUDY_FILE_SUBMISSION_REVIEW = `$${BASE_URL}/api/submission-service/v1/curator/processFiles`;
+export const GET_STUDY_FILE_SUBMISSIONS = `${BASE_URL}/api/submission-service/v1/curator/getSubmissions`;
+export const GET_STUDY_FILE_SUBMISSION_FILES = `${BASE_URL}/api/submission-service/v1/curator/getFilesBySubm?submissionId=`;
+export const POST_STUDY_FILE_SUBMISSION_REVIEW = `${BASE_URL}/api/submission-service/v1/curator/processFiles`;
 export const DOWNLOAD_STUDY_FILES = `/api/submission-service/v1/curator/all-submission-files?submissionId=`;
 export const DOWNLOAD_WEEKLY_REPORT = '/api/report/v1/download/getWeeklyStudyByFileReport?sessionId=';
-export const GET_UPLOAD_PORTAL_DOWNLOADS = `$${BASE_URL}/api/submission-service/v1/uploadPortal/curator/dashboard`;
-export const DELETE_UPLOAD_FILE = `$${BASE_URL}/api/submission-service/v1/uploadPortal/curator/dashboard/delete?uploadId=`;
+export const GET_UPLOAD_PORTAL_DOWNLOADS = `${BASE_URL}/api/submission-service/v1/uploadPortal/curator/dashboard`;
+export const DELETE_UPLOAD_FILE = `${BASE_URL}/api/submission-service/v1/uploadPortal/curator/dashboard/delete?uploadId=`;
 
 // STUDY OVERVIEW
 export const GET_DATA_FILE_CONTENT = `${BASE_URL}/api/download/v1/download/datafile?fileId=`;
@@ -159,8 +159,8 @@ export const GET_NEWS_ARTICLE = `${BASE_URL}/api/entity/v1/getNews/`;
 export const GET_NEWSLETTERS = `${BASE_URL}/api/entity/v1/getNewsletters`;
 
 // STUDY PORTAL
-export const GET_PORTAL_STUDIES = `$${BASE_URL}/api/submission-service/v1/uploadPortal/getStudies`;
-export const UPLOAD_PORTAL_ZIP = `$${BASE_URL}/api/submission-service/v1/uploadPortal/upload`;
+export const GET_PORTAL_STUDIES = `${BASE_URL}/api/submission-service/v1/uploadPortal/getStudies`;
+export const UPLOAD_PORTAL_ZIP = `${BASE_URL}/api/submission-service/v1/uploadPortal/upload`;
 
 /**
  * --------------------------------------------- NEXT JS -----------------------------------------
